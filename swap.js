178b8420bc596f5a90d1421a620c67c368e562d9
Nicolai Hemdrup <21049979+hemdrup@users.noreply.github.com>
2021-12-26 16:28:02 +0100

    Create swap.js

---
+++ swap.js
@@ -0,0 +1,33 @@
+//Untersuchungen
+$("a.swap-link-untersuchungen").attr('href', url + "untersuchungen/" + locationUrl);
+
+//US Kopf-Hals
+$("a.swap-link-kopf").attr('href', url + "kopf/" + locationUrl);
+$("a.swap-link-hals").attr('href', url + "hals/" + locationUrl);
+$("a.swap-link-kiefer").attr('href', url + "kiefer/" + locationUrl);
+
+//US Brustkorb
+$("a.swap-link-brust").attr('href', url + "brust/" + locationUrl);
+$("a.swap-link-herz").attr('href', url + "herz/" + locationUrl);
+
+//US Bauch-becken
+$("a.swap-link-oberbauch").attr('href', url + "oberbauch/" + locationUrl);
+$("a.swap-link-nieren-harnwege").attr('href', url + "nieren-harnwege/" + locationUrl);
+$("a.swap-link-becken").attr('href', url + "becken/" + locationUrl);
+$("a.swap-link-prostata-mpmrt").attr('href', url + "prostata-mpmrt/" + locationUrl);
+
+//US Gelenke
+$("a.swap-link-schulter").attr('href', url + "schulter/" + locationUrl);
+$("a.swap-link-elbogen").attr('href', url + "elbogen/" + locationUrl);
+$("a.swap-link-hand").attr('href', url + "hand/" + locationUrl);
+$("a.swap-link-hufte").attr('href', url + "hufte/" + locationUrl);
+$("a.swap-link-knie").attr('href', url + "knie/" + locationUrl);
+$("a.swap-link-fuss").attr('href', url + "fuss/" + locationUrl);
+
+//US Wirbelsaule
+$("a.swap-link-iliosakralgelenke").attr('href', url + "iliosakralgelenke/" + locationUrl);
+$("a.swap-link-wirbelsaule").attr('href', url + "wirbelsaule/" + locationUrl);
+
+//US Spezial
+$("a.swap-link-darm").attr('href', url + "darm/" + locationUrl);
+$("a.swap-link-gefasse").attr('href', url + "gefasse/" + locationUrl);
